34e6adeb251f47ecb55db9e30db60e770989fc02
MUG <ys8267928563@outlook.jp>
2023-11-28 16:26:46 +0900

    同期処理のスクリプト実装
    既存コンポーネントで使えるものをスクリプトにした時にどういった書き方になるのかを学ぶために一度既存コンポーネントを削除し実装した。

--- Assets/Scripts/SampleTransformView.cs
+++ Assets/Scripts/SampleTransformView.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
-public class SampleTransformView : MonoBehaviour
+public class SampleTransformView : MonoBehaviourPunCallbacks, IPunObservable
 {
-    // Start is called before the first frame update
-    void Start()
+    void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (stream.IsWriting)
+        {
+            // Transformの値をストリームに書き込んで送信する
+            stream.SendNext(transform.localPosition);
+            stream.SendNext(transform.localRotation);
+            stream.SendNext(transform.localScale);
+        }
+        else
+        {
+            // 受信したストリームを読み込んでTransformの値を更新する
+            transform.localPosition = (Vector3)stream.ReceiveNext();
+            transform.localRotation = (Quaternion)stream.ReceiveNext();
+            transform.localScale = (Vector3)stream.ReceiveNext();
+        }
     }
 }
