ac883102574d040e1de058068b695aa324fa2ba0
MUG <ys8267928563@outlook.jp>
2023-11-28 15:52:08 +0900

    日本語フォントの導入

--- Assets/Scripts/SampleScene.cs
+++ Assets/Scripts/SampleScene.cs
@@ -17,8 +17,18 @@ public class SampleScene : MonoBehaviourPunCallbacks
     // マスターサーバーへの接続が成功した時に呼ばれるコールバック
     public override void OnConnectedToMaster()
     {
-        // "Room"という名前のルームに参加する（ルームが存在しなければ作成して参加する）
-        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions(), TypedLobby.Default);
+        // ランダムなルームに参加する
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    // ランダムで参加できるルームが存在しないなら、新規でルームを作成する
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        // ルームの参加人数を2人に設定する
+        var roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 2;
+
+        PhotonNetwork.CreateRoom(null, roomOptions);
     }
 
     // ゲームサーバーへの接続が成功した時に呼ばれるコールバック
@@ -27,5 +37,11 @@ public class SampleScene : MonoBehaviourPunCallbacks
         // ランダムな座標に自身のアバター（ネットワークオブジェクト）を生成する
         Vector3 position = new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f));
         PhotonNetwork.Instantiate("Avatar", position, Quaternion.identity);
+
+        // ルームが満員になったら、以降そのルームへの参加を不許可にする
+        if (PhotonNetwork.CurrentRoom.PlayerCount == PhotonNetwork.CurrentRoom.MaxPlayers)
+        {
+            PhotonNetwork.CurrentRoom.IsOpen = false;
+        }
     }
 }
