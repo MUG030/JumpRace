d56be97ae429ebd75075d060c57822f5f3fc6ef2
MUG <ys8267928563@outlook.jp>
2023-11-30 01:04:32 +0900

    sinemasceneを使った同期カメラの作成
    同期した時に各プレイヤーのカメラを追従するように設定

---
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    private CinemachineVirtualCamera _virtualCamera;
+    private GameObject player;
+
+    void Start()
+    {
+        _virtualCamera = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    private void Update()
+    {
+        if (player == null)
+        {
+            player = GameObject.FindWithTag("Player");
+            if (player != null)
+            {
+                _virtualCamera.Follow = player.transform;
+            }
+        }
+    }
+}
