fccb1521b9e8423cc407773aaf361fedc1e0d374
MUG <ys8267928563@outlook.jp>
2023-11-30 16:36:15 +0900

    部屋待機の作成

--- Assets/Scripts/MatchmakingView.cs
+++ Assets/Scripts/MatchmakingView.cs
@@ -12,8 +12,16 @@ public class MatchmakingView : MonoBehaviourPunCallbacks
     private TMP_InputField passwordInputField = default;
     [SerializeField]
     private Button joinRoomButton = default;
+    [SerializeField]
+    private TextMeshProUGUI statusText;
 
+    private const int MaxPlayerPerRoom = 2;
     private CanvasGroup canvasGroup;
+
+    private void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
 
     private void Start()
     {
@@ -37,7 +45,7 @@ public class MatchmakingView : MonoBehaviourPunCallbacks
     private void OnPasswordInputFieldValueChanged(string value)
     {
         // パスワードを6桁入力した時のみ、ルーム参加ボタンを押せるようにする
-        joinRoomButton.interactable = (value.Length == 6);
+        joinRoomButton.interactable = (value.Length == 1 || value.Length == 6);
     }
 
     private void OnJoinRoomButtonClick()
@@ -45,13 +53,56 @@ public class MatchmakingView : MonoBehaviourPunCallbacks
         // ルーム参加処理中は、入力できないようにする
         canvasGroup.interactable = false;
 
+        var playerCount = PhotonNetwork.CurrentRoom.PlayerCount;
+        if (playerCount != MaxPlayerPerRoom)
+        {
+            statusText.text = "対戦相手を待っています。";
+        }
+        else
+        {
+            statusText.text = "対戦相手が揃いました。バトルシーンに移動します。";
+        }
+
+
         // ルームを非公開に設定する（新規でルームを作成する場合）
         var roomOptions = new RoomOptions();
         roomOptions.MaxPlayers = 2;
         roomOptions.IsVisible = false;
+        if (passwordInputField.text.Length == 6)
+        {
+            if (PhotonNetwork.IsMasterClient)
+            {
+                if (PhotonNetwork.CurrentRoom.PlayerCount == MaxPlayerPerRoom)
+                {
+                    PhotonNetwork.CurrentRoom.IsOpen = false;
+                    statusText.text = "対戦相手が揃いました。バトルシーンに移動します。";
+                    // パスワードと同じ名前のルームに参加する（ルームが存在しなければ作成してから参加する）
+                    PhotonNetwork.JoinOrCreateRoom(passwordInputField.text, roomOptions, TypedLobby.Default);
+                }
+            }
+            
+        } else if (passwordInputField.text.Length == 1)
+        {
+            if (PhotonNetwork.IsMasterClient)
+            {
+                if (PhotonNetwork.CurrentRoom.PlayerCount == MaxPlayerPerRoom)
+                {
+                    PhotonNetwork.CurrentRoom.IsOpen = false;
+                    statusText.text = "対戦相手が揃いました。バトルシーンに移動します。";
+                    PhotonNetwork.JoinRandomRoom();
+                }
+            }
+        }
+    }
 
-        // パスワードと同じ名前のルームに参加する（ルームが存在しなければ作成してから参加する）
-        PhotonNetwork.JoinOrCreateRoom(passwordInputField.text, roomOptions, TypedLobby.Default);
+    // ランダムで参加できるルームが存在しないなら、新規でルームを作成する
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        // ルームの参加人数を2人に設定する
+        var roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 2;
+
+        PhotonNetwork.CreateRoom(null, roomOptions);
     }
 
     public override void OnJoinedRoom()
@@ -71,5 +122,18 @@ public class MatchmakingView : MonoBehaviourPunCallbacks
         // ルームへの参加が失敗したら、パスワードを再び入力できるようにする
         passwordInputField.text = string.Empty;
         canvasGroup.interactable = true;
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            if (PhotonNetwork.CurrentRoom.PlayerCount == MaxPlayerPerRoom)
+            {
+                PhotonNetwork.CurrentRoom.IsOpen = false;
+                statusText.text = "対戦相手が揃いました。バトルシーンに移動します。";
+                PhotonNetwork.LoadLevel("BattleScene");
+            }
+        }
     }
 }
